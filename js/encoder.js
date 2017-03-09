513c8dfa3480afc6763c9e5434644e34f876f554
tdoaty <tdoaty@google.com>
2017-03-09 11:14:19 -0500

    updated

--- js/encoder.js
+++ js/encoder.js
@@ -1,10 +1,13 @@
 var encode = function(userString){
   //1. Add code to turn string to lowercase
-  
+  "encode".toLowerCase()
   // 2. Add code to change each instance of a, e, i, o to the appropriate number based on l33n rules
-
+  "encode".replace(/o/g, "0");
+  "encode".replace(/a/g, "4");
+  "encode".replace(/e/g, "3");
+  "encode."replace(/i/g, "1");
   // 3. Add code to replace spaces with dollar signs.
-
+" ".replace(/ /g, "$");
   //This line assigns the variable encode to the value userString
   return userString
 }
